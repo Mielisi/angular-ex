9866b21854d17b9d151e8648d1afdf36c7718009
mielisi <a.melis003@gmail.com>
2023-11-06 13:01:02 +0100

    WIP-finished the add todo

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -7,13 +7,16 @@ import { Component } from "@angular/core";
 export class AddTodoComponent {
   protected isLoading: boolean;
   protected isPosted: boolean;
+  protected error: string;
 
   constructor() {
     this.isLoading = false;
     this.isPosted = false;
+    this.error = "No error found";
   }
 
   protected async onSubmit(e: Event) {
+    this.error = "No error found";
     e.preventDefault();
     const userElement: HTMLInputElement | null = <HTMLInputElement>(
       document.getElementById("user")
@@ -29,6 +32,31 @@ export class AddTodoComponent {
     );
     const isCompleted: string | null = isCompletedElement.value;
 
-    if (user.trim().length === 0 || title.trim().length === 0) return;
+    if (user.trim().length === 0 || title.trim().length === 0) {
+      user.trim().length === 0 && title.trim().length === 0
+        ? (this.error = " Please insert a title and a user id")
+        : title.trim().length === 0
+        ? (this.error = "Please insert a title")
+        : (this.error = "Please insert the user id ");
+      return;
+    }
+
+    try {
+      this.isLoading = true;
+      const res = await fetch(`https://jsonplaceholder.typicode.com/todos/`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ user, title, isCompleted }),
+      });
+
+      if (!res.ok) throw new Error("Something is wrong");
+      this.isLoading = false;
+      this.isPosted = true;
+      return;
+    } catch (fetchError) {
+      this.isPosted = false;
+      this.error = fetchError as string;
+      return e;
+    }
   }
 }
